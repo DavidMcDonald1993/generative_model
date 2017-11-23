38abef402c70b682d291456fc3ae77d2ead1657f
David McDonald <davemcdonald@live.co.uk>
2017-11-23 17:04:32 +0000

    added facebook script

---
+++ facebook_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH --ntasks 25
+#SBATCH --time 10-00:00:00
+#SBATCH --mem 64000
+
+module purge; module load bluebear
+
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 25 --gamma 2.51017 --T 0.3497
