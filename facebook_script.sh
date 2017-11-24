db860a17449d5d98cfd3aa516bc37c2bcbe96254
David McDonald <davemcdonald@live.co.uk>
2017-11-24 13:33:45 +0000

    2 epochs

--- facebook_script.sh
+++ facebook_script.sh
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 10 --gamma 2.51017 --T 0.3497
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 10 --gamma 2.51017 --T 0.3497 -e 2
