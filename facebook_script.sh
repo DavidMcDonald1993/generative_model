ad44413a9c751ee6b151d0d8b12de871cbbe36be
David McDonald <davemcdonald@live.co.uk>
2017-11-24 13:59:06 +0000

    removed memory profiler

--- facebook_script.sh
+++ facebook_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --ntasks 10
+#SBATCH --ntasks 1
 #SBATCH --time 1-00:00:00
-#SBATCH --mem 100gb
+#SBATCH --mem 32gb
 
 set -e
 
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 10 --gamma 2.51017 --T 0.3497 -e 2
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -e 2
--- src/generative_model.py
+++ src/generative_model.py
@@ -998,7 +998,7 @@ def parse_args():
 	args = parser.parse_args()
 
 	return args
-@profile
+# @profile
 def main():
 
 	args = parse_args()
