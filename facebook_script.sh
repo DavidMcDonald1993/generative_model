60c8f0d5120de1d3285a18b5e7fbd6b16addc536
David McDonald <davemcdonald@live.co.uk>
2017-11-24 17:47:26 +0000

    added one extra core to scripty

--- facebook_script.sh
+++ facebook_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --ntasks 1
+#SBATCH --ntasks 26
 #SBATCH --time 1-00:00:00
-#SBATCH --mem-per-cpu 32gb
+#SBATCH --mem-per-cpu 24gb
 
 set -e
 
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -e 2
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -e 2 -p 25
