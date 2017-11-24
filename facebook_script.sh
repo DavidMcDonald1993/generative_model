c13099900e316d2c1bbbbbfc26ca7d93c35c7b61
David McDonald <davemcdonald@live.co.uk>
2017-11-24 11:57:04 +0000

    changed matplotlib backend

--- facebook_script.sh
+++ facebook_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --ntasks 25
+#SBATCH --ntasks 10
 #SBATCH --time 1-00:00:00
 #SBATCH --mem 100gb
 
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 25 --gamma 2.51017 --T 0.3497
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv -p 10 --gamma 2.51017 --T 0.3497
