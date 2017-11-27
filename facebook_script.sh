47ae72e81d71e8469a761d395df2f39a7748828a
David McDonald <davemcdonald@live.co.uk>
2017-11-27 08:18:21 +0000

    modified script

--- facebook_script.sh
+++ facebook_script.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --ntasks 26
 #SBATCH --time 1-00:00:00
-#SBATCH --mem-per-cpu 24gb
+#SBATCH --mem-per-cpu 32gb
 
 set -e
 
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -e 2 -p 25
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -p 25
