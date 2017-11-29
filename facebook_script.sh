0139f5c084b153e0e47771922cc7bc29bf7306c4
David McDonald <davemcdonald@live.co.uk>
2017-11-29 10:25:50 +0000

    removed parallelism from scripts

--- facebook_script.sh
+++ facebook_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --ntasks 11
-#SBATCH --time 1-00:00:00
-#SBATCH --mem-per-cpu 16gb
+#SBATCH --ntasks 1
+#SBATCH --time 1:00:00
+#SBATCH --mem-per-cpu 8gb
 
 set -e
 
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load apps/python2/2.7.11
 
 
-python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 -p 10 --plot plots/facebook
+python src/generative_model.py data/facebook_graph.gml data/facebook_attributes.csv 193 -c data/facebook_circles.csv --gamma 2.51017 --T 0.3497 --plot plots/facebook 
