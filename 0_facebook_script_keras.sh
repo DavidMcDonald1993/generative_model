04c8c66fe75b3b506b7dfe3a4c0d15cd78144c97
David McDonald <davemcdonald@live.co.uk>
2017-12-01 16:50:28 +0000

    removed gpu

--- 0_facebook_script_keras.sh
+++ 0_facebook_script_keras.sh
@@ -3,13 +3,12 @@
 #SBATCH --ntasks 1
 #SBATCH --time 1-00:00:00
 #SBATCH --mem-per-cpu 8gb
-#SBATCH --qos bbgpu
 
 set -e
 
 module purge; module load bluebear
 module load apps/python2/2.7.11
-module load apps/tensorflow/1.3.1-python-2.7.11-cuda-8.0.44
+module load apps/tensorflow/1.3.1-python-2.7.11
 module load apps/keras/2.0.8-python-2.7.11
 
 
